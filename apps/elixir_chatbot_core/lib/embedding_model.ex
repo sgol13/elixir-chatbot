19caeab402b4c12fa1c9070602259f71f0726273
Karol Wilk <karol.t.wilk@gmail.com>
2023-05-24 22:40:20 +0200

    Add dimension fetch to the EmbeddingModel protocol

--- apps/elixir_chatbot_core/lib/embedding_model.ex
+++ apps/elixir_chatbot_core/lib/embedding_model.ex
@@ -2,5 +2,8 @@ defmodule ElixirChatbotCore.EmbeddingModel do
   defprotocol EmbeddingModel do
     @spec generate_embedding(t, String.t()) :: Nx.Tensor.t()
     def generate_embedding(model, text)
+
+    @spec get_embedding_dimension(t) :: non_neg_integer()
+    def get_embedding_dimension(model)
   end
 end
