65e181712a79b406fa452bd699cf385d403d13dc
Szymon Gołębiowski <szymon13gol@gmail.com>
2023-06-25 10:15:55 +0200

    task 9: initiated Phoenix app ChatWeb

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,77 @@
 # move said applications out of the umbrella.
 import Config
 
+config :chat_web,
+  ecto_repos: [ChatWeb.Repo],
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :chat_web, ChatWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [
+    formats: [html: ChatWeb.ErrorHTML, json: ChatWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: ChatWeb.PubSub,
+  live_view: [signing_salt: "voVcoDLX"]
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.17.11",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../apps/chat_web/assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "3.2.7",
+  default: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../apps/chat_web/assets", __DIR__)
+  ]
+
+config :chat_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :chat_web, ChatWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [
+    formats: [html: ChatWeb.ErrorHTML, json: ChatWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: ChatWeb.PubSub,
+  live_view: [signing_salt: "tg4S49sR"]
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.17.11",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../apps/chat_web/assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "3.2.7",
+  default: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../apps/chat_web/assets", __DIR__)
+  ]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -16,3 +87,14 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
