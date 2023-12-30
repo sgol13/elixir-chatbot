e4a8542d6396ef83a37a70b041f1e882b0696501
Marta <piechowkamarta@gmail.com>
2023-12-30 21:20:22 +0100

    improved markdown

---
+++ apps/chat_web/lib/chat_web/markdown.ex
@@ -0,0 +1,11 @@
+defmodule ChatWeb.Markdown do
+  def to_html(text) do
+    text
+    |> Earmark.as_html!()
+    |> String.replace(~r/<p>/, "<p style=\"padding-top: 5px; padding-bottom: 5px;\">")
+    |> String.replace(~r/<ol>/, "<ol style=\"list-style-type: decimal; list-style-position: inside; display: block; list-style: decimal  outside none; margin: 1em 0; padding: 0 0 0 40px;\">")
+    |> String.replace(~r/<ul>/, "<ul style=\"list-style-type: disc; list-style-position: inside; display: block; list-style: disc  outside none; margin: 1em 0; padding: 0 0 0 40px;\">")
+    |> String.replace(~r/<a/, "<a style=\"text-decoration: underline; color: blue;\"")
+    |> String.replace(~r/<pre>/, "<pre style=\"overflow: auto; overflow-y: hidden;\">")
+  end
+end
